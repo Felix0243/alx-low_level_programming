2edc3e83b530660f88a4551a5da4c61e0f858075
Felix0243 <badu_felix@yahoo.com>
2023-03-06 05:17:49 -0800

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,26 @@
 #include "main.h"
 #include <stdio.h>
 
-void *memset(void *ptr, int value, size_t num);
-int arr[10];
-memset(arr, 0, sizeof(arr));
-memset(ptr, 'A', num);
+/**
+ * _strchr - Locates a character in a string.
+ * @s: string to search
+ * @c: character to locate
+ *
+ * Return: If c is found - a pointer to the first occurence.
+ *         If c is not found - NULL.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+
+	return (NULL);
+}
